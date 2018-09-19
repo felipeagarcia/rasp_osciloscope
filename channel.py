e2a63a33d3eb4da2ec72f3e2da5c17a9bbce41e7
Felipe Garcia <felipegarciaitu@hotmail.com>
2018-09-19 12:07:53 -0300

    Calculating frequency and histogram

--- channel.py
+++ channel.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 from math import pi
 import math
 
@@ -21,6 +22,28 @@ class Channel():
 		plt.grid()
 		plt.show()
 
+	def animated_plot(self):
+		fig, ax = plt.subplots()
+		auto_correlation = np.correlate(self.signal, self.signal, mode='full')
+		# limiting signal by its period
+		y = self.signal[:math.ceil(max(auto_correlation))].copy()
+		x = np.linspace(0, len(y), num=len(y))
+		line, = ax.plot(x, y, self.color)
+
+
+		def animate(i):
+		    line.set_ydata(np.sin(x + i/10.0))  # update the data
+		    return line,
+
+
+		# Init only required for blitting to give a clean slate.
+		def init():
+		    line.set_ydata(np.ma.array(x, mask=True))
+		    return line,
+		ani = animation.FuncAnimation(fig, animate, np.arange(0, len(y)), init_func=init,
+		                              interval=25, blit=True)
+		plt.show()
+
 	def get_signal(self):
 		return self.signal
 
@@ -35,11 +58,45 @@ class Channel():
 
 	def get_period(self):
 		auto_correlation = np.correlate(self.signal, self.signal, mode='full')
-		return math.ceil(max(auto_correlation))
+		return max(auto_correlation)
+
+	def get_frequency(self):
+		return 1/self.get_period()
+
+	def compute_histogram(self, plot=True):
+		histogram, bins = np.histogram(self.signal, density=True)
+		x = np.linspace(0, len(histogram), len(histogram))
+		maximum_val = max(histogram, key=abs)
+		histogram = np.divide(histogram, maximum_val)
+		maximum_val = max(histogram, key=abs)
+		minimum_val = min(histogram, key=abs)
+		base_val, top_val = minimum_val, maximum_val
+		if plot:
+			plt.plot(x, histogram, self.color)
+			plt.title("Signal histogram")
+			plt.grid()
+			plt.show()
+		return base_val, top_val
+
+	def compute_rise_time(self):
+		base_val, top_val = self.compute_histogram(plot=False)
+		print('base', base_val)
+		print('top', top_val)
+		base_time = 'invalid'
+		top_time = 'invalid'
+		for i in range(len(self.signal)):
+			if(self.signal[i] == base_val):
+				base_time = i
+			if(self.signal[i] == top_val*0.90):
+				top_time = i
+		if(base_time == 'invalid' or top_time == 'invalid'):
+			print("Error, rise time cannot be measured")
+		else:
+			return 0.9*top_time - (0.1*top_time + base_time)
 
 	def plot_fft(self, step=0.1):
 		y = np.fft.fft(self.signal)
-		x = np.linspace(-int(len(y)/2), int(len(y)/2))
+		x = np.linspace(-int(len(y)/2), int(len(y)/2), num=len(y))
 		plt.plot(x, y, self.color)
 		plt.xlabel("Frequency (Hz)")
 		plt.ylabel("Amplitude")
--- main.py
+++ main.py
@@ -7,9 +7,11 @@ from channel import Channel
 from math import pi
 
 if __name__ == '__main__':
-	x = np.linspace(0, 4*pi)
+	x = np.linspace(0, 4*pi, num=100)
 	y = np.sin(x)
 	ch1 = Channel(1, 'r-', y)
 	ch1.show()
-	print(ch1.get_max(), ch1.get_min(), ch1.get_peak_to_peak(), ch1.get_period())
+	print(ch1.get_max(), ch1.get_min(), ch1.get_peak_to_peak(), ch1.get_period(), ch1.compute_rise_time())
 	ch1.plot_fft()
+	ch1.animated_plot()
+	ch1.compute_histogram()
