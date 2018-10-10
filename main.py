308c4a602913bb2d6f7e03aeabbe5a8150aa2bd0
Felipe Garcia <felipegarciaitu@hotmail.com>
2018-10-10 12:28:56 -0300

    usb access

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from channel import Channel
 from math import pi
+import string, serial
 
 if __name__ == '__main__':
 	sample_rate = 44.1 * 1000 # 44.1 kHz
@@ -14,3 +15,11 @@ if __name__ == '__main__':
 	print(ch1.get_max(), ch1.get_min(), ch1.get_peak_to_peak(), ch1.get_period())
 	ch1.plot_fft()
 	print('rise_time', ch1.compute_rise_time())
+
+	output = " "
+	ser = serial.Serial('/dev/bus/usb/001/001', 4800, 8, 'N', 1, timeout=1)
+	while True:
+	  print("----")
+	  while output != "":
+	    output = ser.readline()
+	    print (output)
---
+++ user_interface.py
@@ -0,0 +1,6 @@
+
+
+class GUI():
+	def __init__(self, window_height, window_width):
+		self.height = window_height
+		self.width = window_width
